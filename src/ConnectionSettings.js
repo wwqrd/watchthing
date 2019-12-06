2a069e0fee772e522d671e874ed4c9a23360bd92
Steve Mckellar <steve@wwqrd.uk>
2019-12-06 14:51:45 +0000

    Move feedData to hooks/

--- src/ConnectionSettings.js
+++ src/ConnectionSettings.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import useForm from './hooks';
+import useForm from './hooks/useForm';
 
 const ConnectionSettings = ({ settings: initialSettings, onSave }) => {
   const [values, handleChange] = useForm(initialSettings);
